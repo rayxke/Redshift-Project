8d1277de643d9abfbb22b3f976a65629d83af897
Jason <jasonmar3494@gmail.com>
2016-04-08 21:53:17 -0400

    Map Reduce to Sort Tags

---
+++ data/map_reduce_tags.py
@@ -0,0 +1,28 @@
+from mrjob.job import MRJob
+fh = open("tagsMP.txt","w+")
+count = 0
+class MRmyjob(MRJob):
+
+	def mapper(self, _, line):
+		global count
+		wordlist = line.split(" ")
+		count += 1
+
+		for word in wordlist:
+
+			yield word, count
+
+	def reducer(self, key, list_of_values):
+		comb = key.encode('utf-8') + "\t" + str(list(list_of_values)).strip("[]") + "\n"
+		print comb
+		fh.write(comb)
+
+		yield key,list_of_values
+
+if __name__ == '__main__':
+    MRmyjob.run()
+    fh.close()
+
+
+
+
