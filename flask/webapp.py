e6b95ce67b17393df3803124b54e193ef630d9b2
rayxke <keary.d.mobley@gmail.com>
2016-04-17 15:42:37 -0400

    query works with flask

--- flask/webapp.py
+++ flask/webapp.py
@@ -5,8 +5,8 @@
      Authors: Shivam Agarwal, Jason Mar, Keary Mobley, Saketh Poda, Carlos Ramirez, Soma Yamaoka
 '''
 
-from flask import Flask, render_template, url_for
-import json, requests
+from flask import Flask, render_template, url_for, jsonify, request
+import json
 import boto
 import psycopg2
 app = Flask(__name__)
@@ -20,6 +20,14 @@ https://github.com/pallets/flask/tree/master/examples/flaskr
 https://github.com/pallets/flask/tree/master/examples/minitwit
 
 '''
+region = 'us-east-1c'
+#port = 5439
+databasename = "flickrdata"
+username = "jasonmar"
+password = ""
+key_id = ""
+secret_id = ""
+addr = "myrs.cyaakgdtpknb.us-east-1.redshift.amazonaws.com"
 
 #Connects to redshift
 red_conn = boto.connect_redshift(aws_access_key_id=key_id, aws_secret_access_key=secret_id)
@@ -33,9 +41,9 @@ pconn = psycopg2.connect("host= '"+addr+"' port='5439' dbname='"+databasename+"'
     #Close connection to redshift
 
 
-def runQuery():
+def runQuery(tagname):
    #SQL
-   my_query = "select count(*) as modelcount, modelt.modelval as modelval from (select exifmodel.val as modelval, selecttagt.filenum as filenum from  (select tagname as tagname, filenum as filenum from tagsbyfile where tagname = 'computer')selecttagt inner join exifmodel on selecttagt.filenum = exifmodel.filenum)modelt group by modelval order by modelcount desc;"
+   my_query = "select count(*) as modelcount, modelt.modelval as modelval from (select exifmodel.val as modelval, selecttagt.filenum as filenum from  (select tagname as tagname, filenum as filenum from tagsbyfile where tagname ="+"\'"+tagname+"\'"+")selecttagt inner join exifmodel on selecttagt.filenum = exifmodel.filenum)modelt group by modelval order by modelcount desc;"
    cur = pconn.cursor()
    cur.execute(my_query)
    mydict = cur.fetchall()
@@ -49,18 +57,21 @@ def runQuery():
 '''
 Here I plan to implement  the api/ queries?
 '''
-runQuery()
+#runQuery()
 
 @app.route('/')
 def homepage():
-   connectdb()
+   #connectdb()
    return render_template('index.html')
    
 
 
-#@app.route('/search')
+@app.route('/search',methods=['GET'])
+def search_data():
+  tag = request.args.get('Tag')
+  return jsonify(runQuery(tag.lower()))
 
 #Run the app.
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
